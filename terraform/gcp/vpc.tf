c4d9944cc44f9ee6c6e6bfaaf5059fbedc469479
Michael Folorunso <mikebams1194@gmail.com>
2026-06-03 02:33:34 -0600

    feat: add GCP VPC network and GKE subnet

---
+++ terraform/gcp/vpc.tf
@@ -0,0 +1,29 @@
+# GCP VPC Network for NexCloud
+# This creates our private network where all resources will live
+# Custom mode gives us full control over subnets
+
+resource "google_compute_network" "nexcloud_vpc" {
+  name                    = "nexcloud-vpc-${var.environment}"
+  auto_create_subnetworks = false
+  description             = "NexCloud private VPC network"
+}
+
+# Main subnet for GKE cluster
+# This is where our Kubernetes nodes will run
+resource "google_compute_subnetwork" "gke_subnet" {
+  name          = "nexcloud-gke-subnet-${var.environment}"
+  ip_cidr_range = "10.0.0.0/20"
+  region        = var.region
+  network       = google_compute_network.nexcloud_vpc.id
+
+  # Secondary ranges for Kubernetes pods and services
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.48.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.52.0.0/20"
+  }
+}
